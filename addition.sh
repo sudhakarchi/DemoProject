ebced6f47dcf76647255536504f46481d51756a4
sudhakarchi <sudhakarchi3011@gmail.com>
2025-02-14 12:53:17 +0530

    first addition project

---
+++ addition.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+###Enter two numbers
+echo "enter first num"
+read num1
+echo "enter second num"
+read num2
+##addition operation
+sum=$((num1+num2))
+####print ouput
+echo "addition of $num1 and $num2 is:$sum"
+
+
