deaaee5b06e187aeaed79fdc91d4fdebf1ea5059
sudhakarchi <158551884+sudhakarchi@users.noreply.github.com>
2025-03-05 17:15:52 +0530

    Update addition.sh

--- addition.sh
+++ addition.sh
@@ -4,10 +4,13 @@ echo "enter first num"
 read num1
 echo "enter second num"
 read num2
+echo "enter third num"
+read num3
+
 ##addition operation
-sum=$((num1+num2))
+sum=$((num1+num2+num3))
 ####print ouput
-echo "addition of $num1 and $num2 is:$sum"
+echo "addition of $num1 and $num2 and $num3 is:$sum"
 echo "First shellscript"
 
 
